db876edca17e96c654362d9196527900bda37a47
Charlie <cuococarlos@gmail.com>
2018-08-07 11:59:48 -0300

    add rental-detail -WIP

--- src/app/rental-detail/rental-detail.component.ts
+++ src/app/rental-detail/rental-detail.component.ts
@@ -13,6 +13,10 @@ import { UserService } from '../services/user.service'
   styleUrls: ['./rental-detail.component.css']
 })
 export class RentalDetailComponent implements OnInit {
+  isScored: boolean;
+  isReturned: boolean;
+  isIn_USE: boolean;
+  isDone: boolean;
 
   @Input() rental: Rental;
   rentalView;
@@ -74,11 +78,58 @@ export class RentalDetailComponent implements OnInit {
   })
  }
 
+ pay_rental(rental){
+   this.rentalService.getTransaction(rental.id)
+   .subscribe(transaction => {
+     console.log(transaction)
+      this.rentalService.payRental(transaction)
+     .subscribe(transactionEnd =>{
+        console.log(transactionEnd)
+      })
+   ;
+})
+ }
+
+  returnVehicle(rental){
+    this.rentalService.getTransaction(rental.id)
+    .subscribe(transaction => {
+      console.log(transaction)
+       this.rentalService.returnedVehicleRental(transaction)
+      .subscribe(transactionEnd =>{
+         console.log(transactionEnd)
+       })
+    ;
+ })
+  }
+
+  scoredUser(rental){
+    this.rentalService.getTransaction(rental.id)
+    .subscribe(transaction => {
+      console.log(transaction)
+       this.rentalService.payRental(transaction)
+      .subscribe(transactionEnd =>{
+         console.log(transactionEnd)
+       })
+    ;
+ })
+  }
+
+
+
   ngOnInit() {
     this.rentalView = this.rental;
-    this.isOwner = this.rental.ownerCuil === this.userService.getCurrentUserCuil();
-    this.isWaitConfirm = this.rentalView.state === 'WAIT_CONFIRM';
-    this.isConfirm = this.rentalView.state === 'CONFIRM';
+    this.setButtonShowDependsState()
+  }
 
+
+  setButtonShowDependsState(){
+
+    this.isOwner = this.rental.ownerCuil == this.userService.getCurrentUserCuil();
+    this.isWaitConfirm = this.rentalView.state == "WAIT_CONFIRM";
+    this.isConfirm = this.rentalView.state == "CONFIRM";
+    this.isDone = this.rentalView.state == "DONE"
+    this.isIn_USE = this.rentalView.state == "IN_USE"
+    this.isReturned = this.rentalView.state == "RETURNED"
+    this.isScored = this.rentalView.state == "SCORED"
   }
 }
